cedd9ff12aa1b85b7dd5b6efbbc8929dad7abc07
xiangyu <xiangyukongdeveloper@gmail.com>
2022-06-25 16:17:07 -0400

    working on exercices

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -47,17 +47,39 @@ object List: // `List` companion object. Contains functions for creating and wor
   def productViaFoldRight(ns: List[Double]) =
     foldRight(ns, 1.0, _ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] =
+    l match
+      // expect an exception, use sys.error() to throw an exception
+      case Nil => sys.error("tail of empty list")
+      case Cons(_, t) => t
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] =
+    l match
+      case Nil => sys.error("setHead of empty list")
+      case Cons(_, t) => Cons(h, t)
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] =
+    if n <= 0 then l
+    else l match
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n - 1)
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match
+      case Cons(h, t) if f(h) => dropWhile(t, f)
+      case _ => l
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] =
+    l match
+      case Nil => sys.error("init of empty list")
+      case Cons(t, Nil) => Nil
+      case Cons(x, xs) => Cons(x, init(xs))
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int =
+    l match
+      case Nil => 0
+      case Cons(_, t) => length(t) + 1 
+    // foldRight(l, 0, (_,acc) => acc + 1)
 
   def foldLeft[A,B](l: List[A], acc: B, f: (B, A) => B): B = ???
 
